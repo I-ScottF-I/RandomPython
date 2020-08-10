48d1cdc019e56117c3eacabbdf07aaf537790bf7
I-ScottF-I <sferguson@tutamail.com>
2020-08-10 13:01:16 +0100

    creation

---
+++ PF2F5andPA/main.py
@@ -0,0 +1,21 @@
+import csv
+import pprint
+
+def parse_csv(file):
+    #parse CSV into understandable components
+    rules = []
+
+    #open hosts file and read headers
+    with open(file, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            rules.append(row)
+    return rules
+
+file = 'ExampleInput.csv'
+
+#def print_F5_Ouput
+
+output = parse_csv(file)
+pprint.pprint(output)
