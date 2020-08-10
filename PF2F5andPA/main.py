506699fbe352dd2bc995fb0b5ef3502a8e19dc59
I-ScottF-I <66701710+I-ScottF-I@users.noreply.github.com>
2020-08-10 15:04:20 +0100

    Added PA Commands

--- PF2F5andPA/main.py
+++ PF2F5andPA/main.py
@@ -21,8 +21,25 @@ def F5_output(rules):
         outputCommands.append('modify security firewall rule-list <rule-list> rules add { <rule name> { action accept-decisively ip-protocol '+item[2]+' description \'CHANGE NUMBER\' log yes source { addresses add { '+item[0]+'} VLANs add { <VLAN> } } destination { addresses add { '+item[1]+' } ports add { '+item[3]+' } } place-after last } }')
     return outputCommands
 
-#def PA_Output(input):
-#    for item in input:
+def PA_Output(rules):
+    outputCommands = []
+    for item in rules:
+        outputCommands.append('set  rulebase security rules <rule name> profile-setting profiles virus default')
+        outputCommands.append('set  rulebase security rules <rule name> profile-setting profiles spyware default')
+        outputCommands.append('set  rulebase security rules <rule name> profile-setting profiles vulnerability default')
+        outputCommands.append('set  rulebase security rules <rule name> profile-setting profiles wildfire-analysis default')
+        outputCommands.append('set  rulebase security rules <rule name> to «DEST_ZONE»')
+        outputCommands.append('set  rulebase security rules <rule name> from «SRC_ZONE»')
+        outputCommands.append('set  rulebase security rules <rule name> source “[+item[0]+]”')
+        outputCommands.append('set  rulebase security rules <rule name> destination “[+item[1]+]”')
+        outputCommands.append('set  rulebase security rules <rule name> source-user any')
+        outputCommands.append('set  rulebase security rules <rule name> category any')
+        outputCommands.append('set  rulebase security rules <rule name> application any')
+        outputCommands.append('set  rulebase security rules <rule name> service “service”')
+        outputCommands.append('set  rulebase security rules <rule name> hip-profiles any')
+        outputCommands.append('set  rulebase security rules <rule name> action allow')
+        outputCommands.append('set  rulebase security rules <rule name> disabled no')
+
 
 def writeToTxt(commands,device):
     #write data out to csv file
@@ -33,4 +50,4 @@ def writeToTxt(commands,device):
 file = 'ExampleInput.csv'
 output = parse_csv(file)
 writeToTxt(F5_output(output),'f5')
-#writeToTxt(PA_Output(output)))
+writeToTxt(PA_Output(output)))
